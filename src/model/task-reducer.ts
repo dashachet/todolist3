9f6aa5bfef0107ba8ae7e41d0b57c79734227bad
dashachet <drdashashasha@gmail.com>
2025-01-23 19:01:15 +0200

    first commit

---
+++ src/model/task-reducer.ts
@@ -0,0 +1,79 @@
+import {FilterValuesType, TasksStateType, TodolistType} from "../App";
+import {v1} from "uuid";
+
+
+
+const initialState: TasksStateType = {}
+
+export const tasksReducer = (state:  TasksStateType = initialState, action: ActionsType): TasksStateType => {
+    switch (action.type) {
+        case 'REMOVE-TASK': {
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId)
+            }
+        }
+        case 'ADD-TASK': {
+
+            const newTask = {
+                id: v1(),
+                title: action.payload.title,
+                isDone: false
+            }
+            return {...state, [action.payload.todolistId]: [newTask, ...state[action.payload.todolistId]]}
+        }
+        case 'CHANGE_TASK_STATUS':
+         return { ...state,
+            [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id == action.payload.taskId ? {...t, isDone: action.payload.isDone} : t)
+        }
+        case 'CHANGE-TASK-TITLE':
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? {...t, title: action.payload.title} : t)
+            }
+
+        default:
+            return state //  в redux должен вернуться стейт, копия стейта
+    }
+}
+
+// Action creators
+export const removeTodolistAC = (todolistId: string) => {
+    return {type: 'REMOVE-TODOLIST', payload: {id: todolistId}} as const
+}
+export const removeTaskAC = (taskId: string, todolistId: string ) => {
+    return ({
+        type: 'REMOVE-TASK',
+        payload: { taskId, todolistId,} as const
+    })
+}
+
+export const AddTaskAC =(payload: {todolistId:string, title: string})=> {
+    return {
+        type: "ADD-TASK", payload
+    } as const
+
+}
+export const changeTaskStatusAC = (payload: {todolistId: string, taskId: string, isDone: boolean}) => {
+    return ({
+        type: 'CHANGE_TASK_STATUS', payload,
+
+    }) as const
+}
+
+export const  changeTitleStatusAC = (payload: {todolistId: string, taskId: string, title: string})  => {
+    return{ type: 'CHANGE-TASK-TITLE', payload}
+}
+// Actions types
+export type RemoveTodolistActionType = ReturnType<typeof removeTodolistAC>
+
+export type RemoveTaskAT = ReturnType<typeof removeTaskAC>
+
+export type AddTaskAT = ReturnType<typeof AddTaskAC>
+
+export type ChangeTaskStAT= ReturnType<typeof changeTaskStatusAC>
+
+export type changeTitleStatusAT = ReturnType<typeof changeTaskStatusAC>
+type ActionsType = RemoveTodolistActionType | RemoveTaskAT | AddTaskAT | ChangeTaskStAT | changeTitleStatusAT
+
+
---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,67 @@
+import {FilterValuesType, TodolistType} from "../App";
+import {v1} from "uuid";
+
+
+let todolistID1 = v1()
+let todolistID2 = v1()
+
+const initialState: TodolistType[] = [
+	{id: todolistID1, title: 'What to learn', filter: 'all'},
+	{id: todolistID2, title: 'What to buy', filter: 'all'},
+]
+
+export const todolistsReducer = (state: TodolistType[] = initialState, action: ActionsType): TodolistType[] => {
+	switch (action.type) {
+		case 'REMOVE-TODOLIST': {
+			return state.filter(tl => tl.id !== action.payload.id)
+		}
+
+		case 'ADD-TODOLIST': {
+			const todolistId = v1()
+			const newTodolist: TodolistType = {id: todolistId, title: action.payload.title, filter: 'all'}
+			return [...state, newTodolist]
+		}
+
+		case 'CHANGE-TODOLIST-TITLE': {
+			return state.map(tl => tl.id === action.payload.id ? {...tl, title: action.payload.title} : tl)
+		}
+
+		case 'CHANGE-TODOLIST-FILTER': {
+			return state.map(tl => tl.id === action.payload.id ? {...tl, filter: action.payload.filter} : tl)
+		}
+
+		default:
+			return state //  в redux должен вернуться стейт, копия стейта
+	}
+}
+
+// Action creators
+export const removeTodolistAC = (todolistId: string) => {
+	return {type: 'REMOVE-TODOLIST', payload: {id: todolistId}} as const
+}
+
+export const addTodolistAC = (title: string) => {
+	return {type: 'ADD-TODOLIST', payload: {title}} as const
+};
+
+export const changeTodolistTitleAC = (id: string, title: string) => {
+	return {type: 'CHANGE-TODOLIST-TITLE', payload: {id, title}} as const
+};
+
+export const changeTodolistFilter = (id: string, filter: FilterValuesType) => {
+	return {type: 'CHANGE-TODOLIST-FILTER', payload: {id, filter}} as const
+}
+
+// Actions types
+export type RemoveTodolistActionType = ReturnType<typeof removeTodolistAC>
+
+export type AddTodolistActionType = ReturnType<typeof addTodolistAC>
+
+export type ChangeTodolistTitleActionType = ReturnType<typeof changeTodolistTitleAC>
+export type ChangeTodolistFilterActionType = ReturnType<typeof changeTodolistFilter>
+
+type ActionsType = RemoveTodolistActionType
+	| AddTodolistActionType
+	| ChangeTodolistTitleActionType
+	| ChangeTodolistFilterActionType
+
